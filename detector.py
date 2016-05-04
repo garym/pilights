f3885ed713076be11ab7b21d678a2630a813c8e3
Gary Martin <gary.martin@physics.org>
2016-05-04 14:22:12 +0100

    initial brain dump of detector code

---
+++ detector.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python3
+
+from RPi import GPIO
+import time
+
+def detector_event(channel):
+    value = GPIO.input(channel)
+    print('This is an edge event callback.')
+    print('  Edge detected on {}.'.format(channel))
+    print('  This is a {} edge.'.format('rising' if value else 'falling'))
+
+def setup(channel, callback):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.add_event_detect(detector, GPIO.BOTH, callback=callback)
+
+if __name__ == '__main__':
+    setup(4, detector_event)
+    try:
+        while True:
+            time.sleep(1)
+    except Exception:
+        GPIO.cleanup()
