4293215d6268910972333d86575dfdde5446624f
Gary Martin <gary.martin@physics.org>
2016-05-04 14:42:27 +0100

    cleanup on ^C; more meaningful output?

--- detector.py
+++ detector.py
@@ -7,18 +7,24 @@ def detector_event(channel):
     value = GPIO.input(channel)
     print('This is an edge event callback.')
     print('  Edge detected on {}.'.format(channel))
-    print('  This is a {} edge.'.format('rising' if value else 'falling'))
+    print('  Detector is {}.'.format('unblocked' if value else 'blocked'))
 
 def setup(channel, callback):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    print('Initial state of detector is {}.'.format(
+        'unblocked' if GPIO.input(channel) else 'blocked'))
+    print('Waiting for detector events...')
     GPIO.add_event_detect(channel, GPIO.BOTH, callback=callback)
 
 if __name__ == '__main__':
-    setup(4, detector_event)
+    detector = 4
+    setup(detector, detector_event)
     try:
         while True:
             time.sleep(1)
-    except Exception:
+    except KeyboardInterrupt:
+        print('Cleaning up...')
         GPIO.cleanup()
+        print('  done.')
